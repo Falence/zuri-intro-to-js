eb63b42ea4a3b86cbe8fc97f1b1896252fceee86
Falence Lemungoh <falencelemungoh@gmail.com>
2022-06-24 04:15:27 +0100

    Done

---
+++ script.js
@@ -0,0 +1,4 @@
+const fullNames = "Falence Lemungoh";
+const height = "180 cm";
+const country = "Cameroon";
+alert(`Hello, There. I'm ${fullNames}. A ${height} tall software developer from ${country}.`);
